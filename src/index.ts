7f3d45442ec7420a53a930cbb17f1e505e96d3c7
ldyulia <leejulia.dy@gmail.com>
2019-09-04 22:47:49 +0900

    interface test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-const name = "Nicolas",
-  age = 24,
-  gender = "male";
+interface Human {
+  name: string;
+  gender: string;
+  age: number;
+}
 
-const sayHi = (name: string, age: number, gender?: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+const person = {
+  name: "Yulia",
+  gender: "female",
+  age: 22
 };
 
-console.log(sayHi(name, age, "111"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
