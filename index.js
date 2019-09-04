5230528759e6fd96697f9aa0a54061ea57d120f0
ldyulia <leejulia.dy@gmail.com>
2019-09-04 22:33:06 +0900

    tsc-watch에러, typescript 로컬 설치

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Nicolas", age = 24, gender = "male";
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi(name, age, "111"));
-//# sourceMappingURL=index.js.map
---
+++ study/tsc-watch 사용시 에러.md
@@ -0,0 +1,13 @@
+## tsc-watch 사용시 에러
+
+typescript를 global로 설치했더니 tsc-watch세팅 후 yarn start하자 다음과 같은 에러 발생
+
+
+
+Cannot find module 'typescript/bin/tsc'
+
+
+
+검색 후 로컬로 설치하여 해결 (yarn add typescript)
+
+강의에선 글로벌로 설치해서 잘만 쓰던데 뭐가 문젠진 모르겠음ㅎ..
